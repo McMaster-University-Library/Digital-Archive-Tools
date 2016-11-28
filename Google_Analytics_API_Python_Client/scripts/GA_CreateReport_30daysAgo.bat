3e89fdabbfcf97691c914e9521360dade8319d5f
Noel Cochon <cochonnk@mcmaster.ca>
2016-11-28 17:08:19 -0500

    Adding Google Analytics API Python Client folder.

---
+++ Google_Analytics_API_Python_Client/scripts/GA_CreateReport_30daysAgo.bat
@@ -0,0 +1,5 @@
+@setlocal enableextensions
+@cd "C:\Users\cochonnk\Digital-Archive-Tools\Google_Analytics_API_Python_Client\scripts"
+
+python -c "import GA_CreateReport;GA_CreateReport.create_report('30daysAgo')"
+pause
