09b23e1af856ae9e9b229b9bb6e46fd48b25415e
Jay Brodeur <jason.brodeur@gmail.com>
2018-04-03 14:48:35 -0400

    minor update

--- BulkTools/run_DA_metadata_to_mods.m
+++ BulkTools/run_DA_metadata_to_mods.m
@@ -4,3 +4,9 @@ DA_metadata_to_mods('H:\Digitization_Projects\October_Crisis_1970_MCE_topos\','O
 
 cd('D:\Local\Digital-Archive-Tools\BulkTools')
 DA_metadata_to_mods('H:\Digitization_Projects\WWII_AirCharts\WWII_ClothCharts\','Digital Archive - Bulk Metadata Templates - WW2 Cloth Charts.tsv');
+
+cd('D:\Local\Digital-Archive-Tools\BulkTools')
+DA_metadata_to_mods('H:\Digitization_Projects\WWII_Topographic_Maps\Turkey\','Digital Archive - Bulk Metadata Templates - WW2_Turkey_200k_topos.tsv');
+
+cd('D:\Local\Digital-Archive-Tools\BulkTools')
+DA_metadata_to_mods('H:\Digitization_Projects\WWII_Topographic_Maps\Italy\UofA WWII_Italy_Topos\','Digital Archive - Bulk Metadata Templates - UofA WWII Italy Topos.tsv');
