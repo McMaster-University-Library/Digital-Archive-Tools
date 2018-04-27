b60ef4cda071200fbb7c92b307e3e8314ae37b4c
Jay Brodeur <jason.brodeur@gmail.com>
2018-04-27 15:21:56 -0400

    created

---
+++ BulkTools/run_DA_bulk_downloader.m
@@ -0,0 +1,5 @@
+cd('D:\Local\Digital-Archive-Tools\BulkTools\');
+file_ext = '.tiff';
+download_dir = 'H:\Digitization_Projects\WWII_Topographic_Maps\Italy\Italy_100k_TIF_600dpi\';
+download_list = 'H:\Digitization_Projects\WWII_Topographic_Maps\Italy\Italy_100k_TIF_600dpi\macrepos.csv';
+DA_bulk_downloader(file_ext,download_dir,download_list)
